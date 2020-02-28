69ad7039ca2147cbbe572b07d9255827d3795551
Sagar Arora <arorasagar1811@gmail.com>
2020-02-28 15:14:31 -0800

    adding compare method and NAME_COMPARATOR.

--- Student.java
+++ Student.java
@@ -1,9 +1,12 @@
-public class Student {
+public class Student implements Comparable<Student> {
   
   private final long rollNumber;
   private final String firstName;
   private final String lastName;
-  
+  private static final Compartor<Student> NAME_COMPARATOR = 
+    comparing((Student student) -> student.firstName)
+      .thenComparing((Student student) -> student.lastName);
+    
   public Student(long rollNumber, String firstName, String lastName) {
     this.rollNumber = rollNumber;
     this.firstName = firstName;
@@ -29,6 +32,7 @@ public class Student {
     hash = 31 * hash + String.hashCode(lastName);
     return hash;
   }
+  
   /**
    * Returns the string representation of this Student. The String consist of the Student's name and 
    * roll number. for eg. [Student: Sagar Arora rollNumber: 14476353]. 
@@ -37,5 +41,8 @@ public class Student {
   public String toString() {
     return "[Student: " + firstName + " " + lastName + " rollNumber: " + rollNumber + "]"; 
   }
-  
+ 
+  public int compare(Student student) {
+    return Integer.compare(this.rollNumber, student.rollNumber);
+  }
 }
