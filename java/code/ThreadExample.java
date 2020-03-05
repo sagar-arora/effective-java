a3bb85828755af9055a74ac9eafdaba592b258ca
Sagar Arora <arorasagar1811@gmail.com>
2020-03-04 16:14:48 -0800

    creating the thread_example java class.

--- java/code/ThreadExample.java
+++ java/code/ThreadExample.java
@@ -1,4 +1,19 @@
 public class ThreadExample {
-   public class Poet {}
-   public class Poem {}
+    public class Poet extends Runnable{
+        private Poem poem;
+        public Poem(Poem poem) {
+	    this.poem = poem;
+	}
+        appendPoem(Poem poem) {
+	    Poem oldPoem = poem;
+	    
+	}
+    }
+    public class Poem {
+        String poem = poem;
+	
+	Poem(String poem) {
+	    this.poem = poem;
+	}
+    }
 }
